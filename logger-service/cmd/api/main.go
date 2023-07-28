b63d3a2faf1647fea9f0569a065c6b48001246cd
Matheus Batista <matheus.vbatista04@gmail.com>
2023-07-28 01:07:12 -0300

    feature: add logger router to server handler

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"time"
 
+	"github.com/matheus-vb/microservices-go/logger-service/data"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -19,6 +22,7 @@ const (
 var client *mongo.Client
 
 type Config struct {
+	Models data.Models
 }
 
 func main() {
@@ -37,6 +41,24 @@ func main() {
 			panic(err)
 		}
 	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	app.setupServer()
+}
+
+func (app *Config) setupServer() {
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: app.setRoutes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panicln(err)
+	}
 }
 
 func connectToMongo() (*mongo.Client, error) {
