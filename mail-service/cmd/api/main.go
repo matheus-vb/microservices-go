377ed1caca0f5342e74a5720a7f899993be7b4d7
Matheus Batista <matheus.vbatista04@gmail.com>
2023-09-05 23:33:20 -0300

    feature: setup mailer service

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Mailer Mail
+}
+
+const webPort = "80"
+
+func main() {
+	app := Config{
+		Mailer: setupMail(),
+	}
+
+	log.Println("Mail service running")
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.setRoutes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func setupMail() Mail {
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	mail := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+
+	return mail
+}
