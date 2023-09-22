9180da4121f5bb2b76f01328153efeaf980dc9c3
Matheus Batista <matheus.vbatista04@gmail.com>
2023-09-21 21:27:31 -0300

    feature: setup rabbitmq connections

--- broker/src/api/main.go
+++ broker/src/api/main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const port = "80"
 
-type Config struct{}
+type Config struct {
+	Rabbit *amqp.Connection
+}
 
 func main() {
-	app := Config{}
+	rabbitConn, err := connectToMQ()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+
+	log.Println("Connected to RabbitMQ.")
+
+	app := Config{
+		Rabbit: rabbitConn,
+	}
 
 	log.Println("Starting broker service on port ", port)
 
@@ -20,8 +38,38 @@ func main() {
 		Handler: app.setRoutes(),
 	}
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func connectToMQ() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		conn, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			log.Println("RabbitMQ not ready...")
+			counts++
+		} else {
+			connection = conn
+			break
+		}
+
+		if counts > 5 {
+			log.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Backing off...")
+
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
